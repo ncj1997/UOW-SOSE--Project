c5a02f6d1835ed656094c92526ddfc1a042851c2
ncj1997 <nishshankacj@gmail.com>
2023-09-06 22:56:39 +1000

    Initial Commit

---
+++ pages/Scholarship/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-card class="logo py-4 d-flex justify-center">
+          
+          <h1>Heloooooooooooo</h1><br>
+          <div>Hello From Nishshanka</div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </template>
+  
+  <script>
+  export default {
+    name: 'scholarship'
+  }
+  </script>
+  
