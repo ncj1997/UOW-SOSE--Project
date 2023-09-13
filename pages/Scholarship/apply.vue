39ce8e25391481a4d4c00a1a31fa1347288d2fa0
ncj1997 <nishshankacj@gmail.com>
2023-09-13 21:43:34 +1000

    home v1

---
+++ pages/Scholarship/apply.vue
@@ -0,0 +1,124 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title class="headline">Scholarship Application Form</v-card-title>
+      <v-card-text>
+        <v-form ref="form">
+          <!-- Basic Applicant Details -->
+          <v-row>
+            <v-col cols="12" sm="6">
+              <v-text-field
+                v-model="applicant.firstName"
+                label="First Name"
+                required
+                :rules="[(v) => !!v || 'This Field is Required!']"
+                dense
+                outlined
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field
+                v-model="applicant.lastName"
+                label="Last Name"
+                :rules="[(v) => !!v || 'This Field is Required!']"
+                required
+                dense
+                outlined
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="applicant.email"
+                label="Email"
+                :rules="[(v) => !!v || 'This Field is Required!']"
+                required
+                dense
+                outlined
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <!-- CV Upload Section -->
+          <v-row>
+            <v-col cols="6">
+              <v-file-input
+                v-model="applicant.cvFile"
+                :label="applicant.cvFile ? 'CV File Selected' : 'Upload CV'"
+                accept=".pdf,.doc,.docx"
+                :rules="[(v) => !!v || 'This Field is Required!']"
+                required
+                dense
+                outlined
+              ></v-file-input>
+            </v-col>
+            <v-col cols="6">
+              <v-file-input
+                v-model="applicant.recommendationLetterFile"
+                :label="
+                  applicant.recommendationLetterFile
+                    ? 'Recommendation Letter Selected'
+                    : 'Upload Recommendation Letter'
+                "
+                accept=".pdf,.doc"
+                required
+                :rules="[(v) => !!v || 'This Field is Required!']"
+                dense
+                outlined
+              ></v-file-input>
+            </v-col>
+          </v-row>
+
+          <!-- Additional Information -->
+          <v-row>
+            <v-col cols="12">
+              <v-textarea
+                v-model="applicant.additionalInfo"
+                label="Additional Information"
+                dense
+                :rules="[(v) => !!v || 'This Field is Required!']"
+                outlined
+              ></v-textarea>
+            </v-col>
+          </v-row>
+
+          <!-- Submit Button -->
+          <v-row>
+            <v-col cols="12">
+              <v-btn @click="submitApplication" color="primary">Submit</v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<!-- ... rest of the script -->
+<script>
+export default {
+  data() {
+    return {
+      valid: false,
+      applicant: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        cvFile: null,
+        recommendationLetterFile: null,
+        additionalInfo: "",
+      },
+    };
+  },
+  methods: {
+    async validate() {
+      const { valid } = await this.$refs.form.validate();
+      if (valid) alert("Form is valid");
+    },
+    submitApplication() {
+      this.validate();
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,114 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        
-        <h1>Heloooooooooooo</h1><br>
-        <div>Hello From Nishshanka</div>
-      </v-card>
+    <v-col cols="12">
+      <v-carousel
+        cycle
+        height="400"
+        hide-delimiter-background
+        show-arrows="hover"
+      >
+        <!-- <v-carousel-item v-for="(slide, i) in slides" :key="i">
+          <v-sheet :color="colors[i]" height="100%">
+            <div class="d-flex fill-height justify-center align-center">
+              <div class="text-h2">{{ slide }} Slide</div>
+            </div>
+          </v-sheet>
+        </v-carousel-item> -->
+
+        <v-carousel-item
+          src="https://images.unsplash.com/photo-1427504494785-3a9ca7044f45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+          cover
+        ></v-carousel-item>
+
+        <v-carousel-item
+          src="https://images.unsplash.com/photo-1530099486328-e021101a494a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1894&q=80"
+          cover
+        ></v-carousel-item>
+        <v-carousel-item
+          src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80"
+          cover
+        ></v-carousel-item>
+        <v-carousel-item
+          src="https://plus.unsplash.com/premium_photo-1661301105151-fbe357bcfe02?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+          cover
+        ></v-carousel-item>
+      </v-carousel>
+    </v-col>
+
+    <v-col>
+      <v-row justify="center">
+        <v-col v-for="(item, i2) in serviceList" :key="i2">
+          <v-hover v-slot="{ hover }">
+            <v-card
+              :to="item.to"
+              :color="hover ? item.color : undefined"
+              :elevation="hover ? 16 : 2"
+              class="text-center"
+            >
+              <div class="my-2 pt-4">
+                <h3 class="headline mb-0">{{ item.name }}</h3>
+              </div>
+              <v-subheader
+                ><div>{{ item.description }}</div></v-subheader
+              >
+              <v-card-actions>
+                <v-icon class="mx-auto" large>{{ item.icon }}</v-icon>
+              </v-card-actions>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </v-row>
     </v-col>
   </v-row>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+  data() {
+    return {
+      serviceList: [
+        {
+          name: "Postal Service",
+          description:
+            " Fast and reliable mail and package delivery for all your university-related needs.",
+          icon: "mdi-package-variant",
+          color: "#EDE7F6",
+          to: "/postoffice",
+        },
+        {
+          name: "Scholarship Service",
+          description:
+            "Discover opportunities for financial aid and scholarships to support your education.",
+          icon: "mdi-school-outline",
+          color: "#E8EAF6",
+          to: "/scholarship",
+        },
+        {
+          name: "Peer Learning Service:",
+          description:
+            "Collaborate with peers and enhance your learning through workshops and study groups.",
+          icon: "mdi-account-group",
+          color: "#E3F2FD",
+          to: "/peer-learning",
+        },
+      ],
+      colors: [
+        "indigo",
+        "warning",
+        "pink darken-2",
+        "red lighten-1",
+        "deep-purple accent-4",
+      ],
+      slides: ["First", "Second", "Third", "Fourth", "Fifth"],
+    };
+  },
+};
 </script>
+
+<style lang="sass" scoped>
+.v-card.on-hover.v-theme--dark
+  background-color: rgba(#FFF, 0.8)
+  >.v-card__text
+    color: #000
+</style>
