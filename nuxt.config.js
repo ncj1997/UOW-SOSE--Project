4c9b64c48ea1c46a75cf574e3581b54626abf23e
ncj1997 <nishshankacj@gmail.com>
2023-10-27 13:21:27 +1100

    backend added

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: "https://serviceuni-backend.onrender.com/api",
-    baseURL: "http://170.64.149.148:3500/api",
+    baseURL: "https://serviceuni-backend.onrender.com//api",
   },
 
   vue: {
