324d6f055b30e547e95e8abd45114e381fe91d73
ncj1997 <nishshankacj@gmail.com>
2023-10-27 13:23:42 +1100

    bend

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: "https://serviceuni-backend.onrender.com/api",
-    baseURL: "https://serviceuni-backend.onrender.com//api",
+    baseURL: "https://serviceuni-backend.onrender.com/api",
   },
 
   vue: {
